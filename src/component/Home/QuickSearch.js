df80771009759c633e52cc7859dadb0618dda4ba
OmkarDounde <omkardounde1996@gmail.com>
2023-04-11 10:46:32 +0530

    first commit

---
+++ src/component/Home/QuickSearch.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import './QuickSearch.css';
+import QuickDisplay from './QuickDisplay';
+
+// const qurl = "http://3.17.216.66:4000/quicksearch"
+const qurl = "https://zomato-api-op53.onrender.com/mealType"
+
+class QuickSearch extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            mealType:''
+        }
+    }
+    render(){
+        return(
+            <>
+                <div id="quickSearch">
+                    <span id="quickHeading">Quick Search</span>
+                    <span id="quickSubHeading">Discover Restaurants By Meal</span>
+                    <QuickDisplay mealData={this.state.mealType}/>
+                </div>
+            </>
+        )
+    }
+
+    componentDidMount(){
+        fetch(`${qurl}`,{method:'GET'})
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({mealType:data})
+        })
+    }
+    
+}
+
+export default QuickSearch
